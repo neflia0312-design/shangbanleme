95335847768ccb0a53048c2daa22be77112b4ff9
neflia0312-design <neflia0312@gmail.com>
2026-09-10 20:23:17 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shangbanleme-v1.0.0';
+const CACHE='shangbanleme-v1.1.0';
 const ASSETS=['./','./index.html','./art.css','./app.js','./updater.js','./september-2026.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
