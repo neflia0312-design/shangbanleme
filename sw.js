fcae2618a7a82d97dee8de3a4aa1550274c607a0
neflia0312-design <neflia0312@gmail.com>
2026-09-11 10:13:42 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='shangbanleme-v1.3.0';
-const ASSETS=['./','./index.html','./art.css?v=1.3.0','./app.js?v=1.3.0','./updater.js?v=1.3.0','./september-2026.json'];
+const CACHE='shangbanleme-v1.4.0';
+const ASSETS=['./','./index.html','./art.css?v=1.4.0','./app.js?v=1.4.0','./updater.js?v=1.4.0','./september-2026.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('message',event=>{if(event.data==='SKIP_WAITING')self.skipWaiting()});
